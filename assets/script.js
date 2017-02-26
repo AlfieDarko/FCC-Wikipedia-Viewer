d4d6f08ac6381ed9ef3abcc7d7c06ff9bf45d202
Alfie Darko <mrhazzy@gmail.com>
2017-02-26 16:36:25 +0000

    worked on functionality and debugs :wq

--- assets/script.js
+++ assets/script.js
@@ -1,22 +1,32 @@
 $(document).ready(function () {
-    //your code here
     var searchTerm;
     var url;
-    $("#red").keyup(function () {
-        
-        var searchTerm = $("#red").val();
-        var url = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + searchTerm + "&limit=10&namespace=0&format=json";
-        
+    $("#subme").submit(function () {
 
+        searchTerm = $("#red").val();
         
-        $.getJSON(url, function (wiki) {
-            $("#wikiResultsContainer").append(
-            
-            "<div><h3>wiki[0]</h3></div>"
-            
-            
-            );
+         url = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + searchTerm + "&limit=10&namespace=0&format=json";
+        
+        console.log(searchTerm)
+        
+        $.getJSON(url, function () {
+            $("#wikiResultsContainer").append(searchTerm);
+            console.log("done");
+        }).done(function () {
+            console.log("success");
         });
-        console.log(url);
+        return false;
     });
 });
+
+
+//console.log(url);
+//console.log(searchTerm);
+
+//$.getJSON(url, function () {
+//    $("#wikiResultsContainer").append(searchTerm);
+//    console.log("done");
+//}).done(function () {
+//    $("wikiResultsContainer").append(searchTerm);
+//    console.log("success");
+//});
